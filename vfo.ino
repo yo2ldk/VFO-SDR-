f7bbd596db09c23a553902cd5254cc69f4dd3d33
alexander - yo2ldk <yo2ldk@yahoo.com>
2015-12-13 15:41:00 +0200

    VFO.ino

---
+++ vfo.ino
@@ -0,0 +1,45 @@
+#include <TrinketHidCombo_m.h>
+#define VFO_UP   2
+#define VFO_DOWN 0
+#define MODE     5
+
+static uint8_t enc_prev_pos   = 0;
+static uint8_t enc_flags      = 0;
+void setup()
+{  
+   pinMode(VFO_UP, INPUT);
+   pinMode(VFO_DOWN, INPUT);
+   pinMode(MODE, INPUT);
+
+   digitalWrite(VFO_UP, LOW);
+   digitalWrite(VFO_DOWN, LOW);
+   //digitalWrite(MODE, LOW);
+  
+  TrinketHidCombo.begin(); 
+}
+
+void loop(){
+  if(!digitalRead(VFO_UP) ==HIGH) {
+     TrinketHidCombo.pressKey(0x01,0x51);
+     TrinketHidCombo.pressKey(0,0);
+    delay(10); 
+    }else{
+   }
+    
+   
+  if(!digitalRead(VFO_DOWN) ==HIGH) {
+      TrinketHidCombo.pressKey(0x01,0x52);
+      TrinketHidCombo.pressKey(0,0);
+     delay(10);
+    // }else{
+   //}
+   
+ // if(!digitalRead(MODE) ==HIGH) {
+     // TrinketHidCombo.pressKey(0x01,0x19);
+     // TrinketHidCombo.pressKey(0,0);
+     // delay(10);
+    
+   }
+    
+    TrinketHidCombo.poll(); 
+}
